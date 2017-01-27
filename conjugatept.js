7afb4dee0c7d3e189b4a1cc1a6dbc8c31cfd4c71
Anthony Liu <anthonyscottliu@gmail.com>
2017-01-26 20:01:10 -0500

    Reads the words from input files.

--- conjugatept.js
+++ conjugatept.js
@@ -2,4 +2,26 @@
 // @author Anthony Liu <igliu@mit.edu>
 // @version 0.0.0
 
-console.log('conjugate-pt');
+// dependencies
+var fs = require('fs');
+var request = require('request');
+
+// config
+var endpoint = 'http://www.conjuga-me.net/en/verbo-';
+
+// check arguments
+if (process.argv.length < 4) {
+  return console.log(
+    'Usage: node conjugatept.js IN_FILE OUT_FILE'
+  );
+}
+
+// get arguments
+var inFile = process.argv[2];
+var outFile = process.argv[3];
+
+// load the words
+var inputFileContents = fs.readFileSync(inFile, 'UTF-8');
+var words = inputFileContents.split('\n').slice(0, -1);
+
+console.log(words);
